d6311ef227d7a6de1f39c9f49ef10daf714e165c
chxxing <choing26@naver.com>
2022-08-12 16:07:53 +0900

    220812

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 $(function () {
     $('.main_slider').slick({
         arrows: false,
+        dots: true,
         pauseOnFocus: false,
         pauseOnHover: false,
-        dots: true,
-    })
+    });
 })
