861b263d774b922c5b781c4074e14193b8eda30f
chxxing <choing26@naver.com>
2022-08-17 17:53:06 +0900

    220817

--- js/main.js
+++ js/main.js
@@ -7,4 +7,20 @@ $(function () {
         autoplay: true,
         autoplaySpeed: 3500,
     });
+
+    $('.eve_slider').slick({
+        arrows: false,
+        pauseOnFocus: false,
+        pauseOnHover: false,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+    })
+
+    $('.eve_arrows i:nth-child(1)').on('click', function () {
+        $('.eve_slider').slick('slickPrev')
+    })
+
+    $('.eve_arrows i:nth-child(2)').on('click', function () {
+        $('.eve_slider').slick('slickNext')
+    })
 })
