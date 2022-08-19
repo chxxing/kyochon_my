dc31cf2046805730a646e2737754444eee7f14d2
최은진 <105770718+chxxing@users.noreply.github.com>
2022-08-19 14:04:09 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -6,14 +6,32 @@ $(function () {
         pauseOnHover: false,
         autoplay: true,
         autoplaySpeed: 3500,
+        responsive: [{
+            breakpoint: 768,
+            settings: {
+                arrow: false,
+                slidesToShow: 1,
+                slidesToScroll: 1,
+            }
+        }]
     });
 
     $('.eve_slider').slick({
         arrows: false,
-        pauseOnFocus: false,
-        pauseOnHover: false,
-        slidesToShow: 3,
+        slidesToShow: 2,
         slidesToScroll: 1,
+        responsive: [{
+            breakpoint: 768,
+            settings: {
+                autoplay: true,
+                autoplaySpeed: 4000,
+                centerMode: false,
+                pauseOnFocus: false,
+                pauseOnHover: false,
+                slidesToShow: 1,
+                slidesToScroll: 1,
+            }
+        }]
     })
 
     $('.eve_arrows i:nth-child(1)').on('click', function () {
@@ -23,4 +41,38 @@ $(function () {
     $('.eve_arrows i:nth-child(2)').on('click', function () {
         $('.eve_slider').slick('slickNext')
     })
+
+    $('.news_slider').slick({
+        arrows: false,
+        centerMode: true,
+        centerPadding: '5px',
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        responsive: [{
+            breakpoint: 768,
+            settings: {
+                autoplay: true,
+                centerMode: false,
+                pauseOnFocus: false,
+                pauseOnHover: false,
+                slidesToShow: 1,
+                slidesToScroll: 1,
+            }
+        }]
+    })
+
+    $('.news_arrows i:nth-child(1)').on('click', function () {
+        $('.news_slider').slick('slickPrev')
+    })
+
+    $('.news_arrows i:nth-child(2)').on('click', function () {
+        $('.news_slider').slick('slickNext')
+    })
+
 })
+
+AOS.init({
+    delay: 100,
+    duration: 500,
+});
+
