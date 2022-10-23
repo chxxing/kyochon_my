be9e41b1bc44a11f8d033955f67dd51e0900f9b1
chxxing <choing26@naver.com>
2022-10-24 02:22:38 +0900

    221024

--- js/main.js
+++ js/main.js
@@ -1,4 +1,38 @@
 $(function () {
+    $('.hamburger').on('click', function () {
+        $(this).toggleClass('is-active');
+        $('.gnb').toggleClass('on');
+        if ($('.gnb').hasClass('on')) {
+            $('html, body').addClass('noScroll');
+            $('.main_slider').slick('slickPause');
+        } else {
+            $('html, body').toggleClass('noScroll');
+            $('.main_slider').slick('slickPlay');
+        };
+    });
+
+    $('.gnb>ul>li>a').on('click', function (e) {
+        if ($('.gnb').hasClass('on')) {
+            e.preventDefault();
+            $('.smenu').slideUp();
+            $(this).next().stop().slideToggle();
+        }
+    });
+
+    $(window).on('resize', function () {
+        $('.gnb').removeClass('on');
+        $('.smenu').removeAttr('style');
+    });
+
+    $(window).on("scroll", function () {
+        var sct = $(window).scrollTop();
+        if (sct > 0) {
+            $(".header .bottom_menu").addClass("on");
+        } else {
+            $(".header .bottom_menu").removeClass("on");
+        }
+    });
+
     $('.main_slider').slick({
         arrows: false,
         dots: true,
@@ -23,8 +57,6 @@ $(function () {
         responsive: [{
             breakpoint: 768,
             settings: {
-                autoplay: true,
-                autoplaySpeed: 4000,
                 centerMode: false,
                 pauseOnFocus: false,
                 pauseOnHover: false,
@@ -51,12 +83,13 @@ $(function () {
         responsive: [{
             breakpoint: 768,
             settings: {
-                autoplay: true,
+                autoplay: false,
                 centerMode: false,
+                centerPadding: '0px',
                 pauseOnFocus: false,
                 pauseOnHover: false,
-                slidesToShow: 1,
-                slidesToScroll: 1,
+                slidesToShow: 2,
+                slidesToScroll: 2,
             }
         }]
     })
@@ -69,10 +102,8 @@ $(function () {
         $('.news_slider').slick('slickNext')
     })
 
-})
-
-AOS.init({
-    delay: 100,
-    duration: 500,
+    AOS.init({
+        delay: 120,
+        duration: 500,
+    });
 });
-
